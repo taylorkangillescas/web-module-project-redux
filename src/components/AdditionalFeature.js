5f21f8b97c2d6a2065245da61368a00fb4b268ad
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-04-16 13:16:29 -0700

    updated2

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import { addFeature } from '../actions/carSalesActions'
+import { connect } from 'react-redux'
+ 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick ={props.addFeature} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, { addFeature })(AdditionalFeature);
